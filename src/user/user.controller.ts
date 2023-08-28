d73b0f233d8f15536247cd0410096d2236893a30
Jeremy Paule Pereira <jeremy.devpaulepereira@gmail.com>
2023-08-28 15:31:09 -0300

    feature: Connecting database and creating entities

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,18 +1,19 @@
 import { Body, Controller, Get, Post } from '@nestjs/common';
 import { CreateUserDto } from './dto/createUser.dto';
+import { UserService } from './user.service';
+import { UserEntity } from './interfaces/user.entity';
 
 @Controller('user')
 export class UserController {
+  constructor(private readonly userService: UserService) {}
+
   @Get()
-  async getAllUsers() {
-    return JSON.stringify({ test: 'abc' });
+  async getAllUsers(): Promise<UserEntity[]> {
+    return this.userService.getAllUsers();
   }
 
   @Post()
-  async createUser(@Body() createUser: CreateUserDto) {
-    return {
-      ...createUser,
-      password: undefined,
-    };
+  async createUser(@Body() createUser: CreateUserDto): Promise<UserEntity> {
+    return this.userService.createUser(createUser);
   }
 }
---
+++ src/user/user.service.ts
@@ -0,0 +1,31 @@
+import { Injectable } from '@nestjs/common';
+import { CreateUserDto } from './dto/createUser.dto';
+import { UserEntity } from './interfaces/user.entity';
+import * as bcrypt from 'bcrypt';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+
+@Injectable()
+export class UserService {
+  constructor(
+    @InjectRepository(UserEntity)
+    private readonly userRepository: Repository<UserEntity>,
+  ) {}
+
+  async getAllUsers(): Promise<UserEntity[]> {
+    return this.userRepository.find();
+  }
+
+  async createUser(createUserDto: CreateUserDto): Promise<UserEntity> {
+    const saltOrRounds = 10;
+    const hashPassword = await bcrypt.hash(
+      createUserDto.password,
+      saltOrRounds,
+    );
+
+    return this.userRepository.save({
+      ...createUserDto,
+      password: hashPassword,
+    });
+  }
+}
