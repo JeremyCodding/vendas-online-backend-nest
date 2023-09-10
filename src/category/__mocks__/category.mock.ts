2a9212899e17f46b8e3e0c34c6f29b7a66d68237
Jeremy Paule Pereira <jeremy.devpaulepereira@gmail.com>
2023-09-10 16:32:55 -0300

    feature: create categoryService

---
+++ src/category/__mocks__/category.mock.ts
@@ -0,0 +1,8 @@
+import { CategoryEntity } from '../entities/category.entity';
+
+export const categoryMock: CategoryEntity = {
+  createdAt: new Date(),
+  updatedAt: new Date(),
+  id: 1,
+  name: 'Mock Category',
+};
