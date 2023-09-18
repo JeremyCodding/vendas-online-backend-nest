a3b0cbceb7ce4474e03dd2379c3147c58cff542b
Jeremy Paule Pereira <jeremy.devpaulepereira@gmail.com>
2023-09-17 22:04:55 -0300

    feature: Delete product implemented

--- src/product/product.service.ts
+++ src/product/product.service.ts
@@ -1,7 +1,7 @@
 import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { ProductEntity } from './entities/product.entity';
-import { Repository } from 'typeorm';
+import { DeleteResult, Repository } from 'typeorm';
 import { CreateProduct } from './dto/create-product.dto';
 import { CategoryService } from '../category/category.service';
 
@@ -27,5 +27,27 @@ export class ProductService {
     await this.categoryService.findCategoryById(createProduct.categoryId);
 
     return this.productRepository.save({ ...createProduct });
+  }
+
+  async findProductById(productId: number): Promise<ProductEntity> {
+    const product = await this.productRepository.findOne({
+      where: {
+        id: productId,
+      },
+    });
+
+    if (!product) {
+      throw new NotFoundException(
+        `O produto de ID ${productId} não foi encontrado`,
+      );
+    }
+
+    return product;
+  }
+
+  async deleteProduct(productId: number): Promise<DeleteResult> {
+    await this.findProductById(productId);
+
+    return this.productRepository.delete({ id: productId });
   }
 }
