1508e94df39685238b66e1654055761fb0b86d47
Jeremy Paule Pereira <jeremy.devpaulepereira@gmail.com>
2023-09-23 16:15:46 -0300

    feature: Insert product in cart created

--- src/cart/cart.controller.ts
+++ src/cart/cart.controller.ts
@@ -1,7 +1,29 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Post,
+  UsePipes,
+  ValidationPipe,
+} from '@nestjs/common';
 import { Roles } from 'src/decorators/roles.decorator';
 import { UserType } from 'src/user/enum/user-type.enum';
+import { CartEntity } from './entities/cart.entity';
+import { InserCartDTO } from './dto/insert-cart.dto';
+import { CartService } from './cart.service';
+import { UserId } from 'src/decorators/user-id.decorator';
 
 @Roles(UserType.User)
 @Controller('cart')
-export class CartController {}
+export class CartController {
+  constructor(private readonly cartService: CartService) {}
+
+  @Roles(UserType.Admin, UserType.User)
+  @UsePipes(ValidationPipe)
+  @Post()
+  async createCart(
+    @Body() insertCart: InserCartDTO,
+    @UserId() userId: number,
+  ): Promise<CartEntity> {
+    return this.cartService.insertProductInCart(insertCart, userId);
+  }
+}
